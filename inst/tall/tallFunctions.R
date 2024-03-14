9c691667f8044ae783cdecda96cb039a3df0d64c
Massimo Aria <aria@unina.it>
2024-03-14 17:09:44 +0100

    resort values in topic correlation plot

--- inst/tall/tallFunctions.R
+++ inst/tall/tallFunctions.R
@@ -801,6 +801,51 @@ valueBoxesIndices <- function(x){
   )
 }
 
+## wordcloud function
+wordcloud2a <- function (data, size = 1, minSize = 0, gridSize = 0, fontFamily = "Segoe UI",
+                         fontWeight = "bold", color = "random-dark", backgroundColor = "transparent",
+                         minRotation = -pi/4, maxRotation = pi/4, shuffle = TRUE,
+                         rotateRatio = 0.4, shape = "circle", ellipticity = 0.65,
+                         widgetsize = NULL, figPath = NULL, hoverFunction = NULL)
+{
+  if ("table" %in% class(data)) {
+    dataOut = data.frame(name = names(data), freq = as.vector(data))
+  }
+  else {
+    data = as.data.frame(data)
+    dataOut = data[, 1:2]
+    names(dataOut) = c("name", "freq")
+  }
+  if (!is.null(figPath)) {
+    if (!file.exists(figPath)) {
+      stop("cannot find fig in the figPath")
+    }
+    spPath = strsplit(figPath, "\\.")[[1]]
+    len = length(spPath)
+    figClass = spPath[len]
+    if (!figClass %in% c("jpeg", "jpg", "png", "bmp", "gif")) {
+      stop("file should be a jpeg, jpg, png, bmp or gif file!")
+    }
+    base64 = base64enc::base64encode(figPath)
+    base64 = paste0("data:image/", figClass, ";base64,",
+                    base64)
+  }
+  else {
+    base64 = NULL
+  }
+  weightFactor = size * 180/max(dataOut$freq)
+  settings <- list(word = dataOut$name, freq = dataOut$freq,
+                   fontFamily = fontFamily, fontWeight = fontWeight, color = color,
+                   minSize = minSize, weightFactor = weightFactor, backgroundColor = backgroundColor,
+                   gridSize = gridSize, minRotation = minRotation, maxRotation = maxRotation,
+                   shuffle = shuffle, rotateRatio = rotateRatio, shape = shape,
+                   ellipticity = ellipticity, figBase64 = base64, hover = htmlwidgets::JS(hoverFunction))
+  chart = htmlwidgets::createWidget("wordcloud2", settings,
+                                    width = widgetsize[1], height = widgetsize[2], sizingPolicy = htmlwidgets::sizingPolicy(viewer.padding = 0,
+                                                                                                                            browser.padding = 0, browser.fill = TRUE))
+  chart
+}
+
 ## TFIDF functions ----
 tfidf <- function(dfTag, term="lemma", document="doc_id"){
   # calculate tfidf
@@ -2007,10 +2052,14 @@ tmHeatmap <- function(beta){
   data <- cor(as.matrix(beta[,-1]))
   diag(data) <- 0
 
-  df = data.frame(data)
-  colnames(df) <- row.names(df) <- paste0("topic ",colnames(beta)[-1])
+  data <- data[nrow(data):1,]
 
-  x <- y <- row.names(df)
+  df = data.frame(data)
+  # x <- y <- colnames(df) <- row.names(df) <- paste0("topic ",nrow(data):1)
+  y <- row.names(df) <- paste0("topic ",nrow(data):1)
+
+  x <- colnames(df) <- sort(y)
+
   df <- df %>%
     rownames_to_column("y") %>%
     pivot_longer(cols = starts_with("topic "), names_to = "variable", values_to = "value") %>%
@@ -2029,8 +2078,7 @@ tmHeatmap <- function(beta){
     colors = pal,
     zauto = F,
     zmin=-1,
-    zmax=1,
-    zmed=0) %>%
+    zmax=1) %>%
     add_annotations(
       data = df,
       x = ~variable,
